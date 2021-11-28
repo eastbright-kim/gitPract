915b6a18b1e7fd9308230d92bc074d745c1e3ad8
김동환 <1033513@naver.com>
2021-11-28 23:29:40 +0900

    newnewbranch

--- gitPract/ViewController.swift
+++ gitPract/ViewController.swift
@@ -12,10 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //branch commit
-        //branch commit 1
-        //barnch 2
-        //branch 3
+        //complete new branch
     }
 
 
