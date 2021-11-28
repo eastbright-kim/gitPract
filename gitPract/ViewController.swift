60fab4d7f018001695dc261109cdf5b3ae812d6d
김동환 <1033513@naver.com>
2021-11-28 23:30:08 +0900

    newenwenwe

--- gitPract/ViewController.swift
+++ gitPract/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //complete new branch
+        // new commit to new branch
     }
 
 
