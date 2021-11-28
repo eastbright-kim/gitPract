82d87f73cb6bdf7c3216a2ef85c989ac1ea71428
김동환 <1033513@naver.com>
2021-11-28 22:59:59 +0900

    또 다른 브랜치

--- gitPract/ViewController.swift
+++ gitPract/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         //branch commit
         //branch commit 1
+        //barnch 2
     }
 
 
